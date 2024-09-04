cf7a3ac11d882d167845baa51db70d0571a79b73
Sam <samjon@inbox.ru>
2024-09-04 18:14:40 +0300

    1 коммит

---
+++ module_7_2.py
@@ -0,0 +1,21 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for string in strings:
+            position = file.tell()
+            file.write(string + '\n')
+            strings_positions[(len(strings_positions) + 1, position)] = string
+    return strings_positions
+
+# Пример использования
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('file_name.txt', info)
+for elem in result.items():
+    print(elem)
+
