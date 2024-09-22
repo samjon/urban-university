f6d0f58571363b8c6f82589ddb74ea8a8301f4d5
Sam <samjon@inbox.ru>
2024-09-22 19:38:24 +0300

    1 коммит

---
+++ module_10_1.py
@@ -0,0 +1,49 @@
+import time
+from time import sleep
+from threading import Thread
+
+# Определяем функцию write_words
+def write_words(word_count, file_name):
+    with open(file_name, 'w') as file:
+        for i in range(1, word_count + 1):
+            file.write(f"Какое-то слово № {i}\n")
+            sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+# Функция для измерения времени выполнения
+def measure_time(func, *args, **kwargs):
+    start_time = time.monotonic()
+    result = func(*args, **kwargs)
+    end_time = time.monotonic()
+    return end_time - start_time
+
+# Последовательные вызовы функции
+start_seq = time.monotonic()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+end_seq = time.monotonic()
+
+# Вывод времени выполнения последовательных вызовов
+print(f"Последовательная работа: {end_seq - start_seq}")
+
+# Создание и запуск потоков
+threads = [
+    Thread(target=write_words, args=(10, 'example5.txt')),
+    Thread(target=write_words, args=(30, 'example6.txt')),
+    Thread(target=write_words, args=(200, 'example7.txt')),
+    Thread(target=write_words, args=(100, 'example8.txt'))
+]
+
+start_concur = time.monotonic()
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+end_concur = time.monotonic()
+
+# Вывод времени выполнения параллельных вызовов
+print(f"Работа потоков {end_concur - start_concur}")
+
