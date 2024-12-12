aff641b11fb6482cb6527a82d992855b56cba511
samjon <samjon@inbox.ru>
2024-12-12 18:59:36 +0300

    Обновлённая версия тестов

--- module_12_1.py
+++ module_12_1.py
@@ -1,44 +1,46 @@
+from rt_with_exceptions import Runner
 import unittest
+import logging
 
 
-class Runner:
-    def __init__(self, name):
-        self.name = name
-        self.distance = 0
+class TestRunner(unittest.TestCase):
+    is_frozen = False
 
-    def run(self):
-        self.distance += 10
-
-    def walk(self):
-        self.distance += 5
-
-    def __str__(self):
-        return self.name
-
-
-class RunnerTest(unittest.TestCase):
-
-    def test_walk(self):
-        runner = Runner("Test Runner")
-        for _ in range(10):
-            runner.walk()
-        self.assertEqual(runner.distance, 50)
-
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
-        runner = Runner("Test Runner")
-        for _ in range(10):
-            runner.run()
-        self.assertEqual(runner.distance, 100)
+        try:
+            self.runner = Runner("Test Runner", 50)
+            for i in range(10):
+                self.runner.run()
+            self.assertEqual(self.runner.distance, 1000, 'Test Runner.run() - failed!')
+            logging.info('"test_run" выполнен успешно')
+        except TypeError:
+            logging.warning('Неверный тип данных для объекта Runner', exc_info=True)
+        except ValueError:
+            logging.warning('Неверное значение скорости для объекта Runner', exc_info=True)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        try:
+            self.runner = Runner('test', 10)
+            for i in range(10):
+                self.runner.walk()
+            self.assertEqual(self.runner.distance, 100, 'Test Runner.walk() - failed!')
+            logging.info('"test_walk" выполнен успешно')
+        except TypeError:
+            logging.warning('Неверный тип данных для объекта Runner', exc_info=True)
+        except ValueError:
+            logging.warning('Неверное значение скорости для объекта Runner', exc_info=True)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
-        runner1 = Runner("Runner One")
-        runner2 = Runner("Runner Two")
-
-        for _ in range(10):
-            runner1.run()
-            runner2.walk()
-
-        self.assertNotEqual(runner1.distance, runner2.distance)
+        self.runner = Runner('test')
+        test_obj2 = Runner('test2')
+        for i in range(10):
+            self.runner.walk()
+            test_obj2.run()
+        self.assertNotEqual(self.runner.distance, test_obj2.distance,
+                            'Combined test Runner.walk() & Runner.run()- failed!')
 
 
 if __name__ == '__main__':
--- module_12_2.py
+++ module_12_2.py
@@ -1,80 +1,48 @@
+from runner_and_tournament import Runner, Tournament
 import unittest
 
-class Runner:
-    def __init__(self, name, speed=5):
-        self.name = name
-        self.distance = 0
-        self.speed = speed
 
-    def run(self):
-        self.distance += self.speed * 2
-
-    def walk(self):
-        self.distance += self.speed
-
-    def __str__(self):
-        return self.name
-
-    def __eq__(self, other):
-        if isinstance(other, str):
-            return self.name == other
-        elif isinstance(other, Runner):
-            return self.name == other.name
-
-
-class Tournament:
-    def __init__(self, distance, *participants):
-        self.full_distance = distance
-        self.participants = list(participants)
-
-    def start(self):
-        finishers = {}
-        place = 1
-        while self.participants:
-            for participant in self.participants:
-                participant.run()
-                if participant.distance >= self.full_distance:
-                    finishers[place] = participant
-                    place += 1
-                    self.participants.remove(participant)
-
-        return finishers
-
-
-class TournamentTest(unittest.TestCase):
-    all_results = {}
+class TestTournament(unittest.TestCase):
+    is_frozen = True
 
     @classmethod
     def setUpClass(cls):
-        cls.all_results = {}
-
-    def setUp(self):
-        self.runner1 = Runner("Усэйн", speed=10)
-        self.runner2 = Runner("Андрей", speed=9)
-        self.runner3 = Runner("Ник", speed=3)
+        cls.all_results = []
 
     @classmethod
     def tearDownClass(cls):
-        for result in cls.all_results.values():
+        for result in cls.all_results:
             print(result)
 
-    def test_race_usain_nik(self):
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def setUp(self):
+        self.runner1 = Runner('Усэйн', 10)
+        self.runner2 = Runner('Андрей', 9)
+        self.runner3 = Runner('Ник', 3)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test1(self):
         tournament = Tournament(90, self.runner1, self.runner3)
-        results = tournament.start()
-        self.all_results[max(results.keys())] = results[max(results.keys())].name
-        self.assertTrue(self.all_results[max(results.keys())] == "Ник")
+        result = tournament.start()
+        self.assertEqual(result[len(result)].name, self.runner3.name, 'Test for last runner - failed!')
+        self.assertEqual(result[1].name, self.runner1.name, 'Test for first runner - failed!')
+        self.all_results.append(result)
 
-    def test_race_andrey_nik(self):
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test2(self):
         tournament = Tournament(90, self.runner2, self.runner3)
-        results = tournament.start()
-        self.all_results[max(results.keys())] = results[max(results.keys())].name
-        self.assertTrue(self.all_results[max(results.keys())] == "Ник")
+        result = tournament.start()
+        self.assertEqual(result[len(result)].name, self.runner3.name, 'Test for last runner - failed!')
+        self.assertEqual(result[1].name, self.runner2.name, 'Test for first runner - failed!')
+        self.all_results.append(result)
 
-    def test_race_usain_andrey_nik(self):
-        tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
-        results = tournament.start()
-        self.all_results[max(results.keys())] = results[max(results.keys())].name
-        self.assertTrue(self.all_results[max(results.keys())] == "Ник")
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test3(self):
+        tournament = Tournament(90, self.runner2, self.runner1, self.runner3)
+        result = tournament.start()
+        self.assertEqual(result[len(result)].name, self.runner3.name, 'Test for last runner - failed!')
+        self.assertEqual(result[1].name, self.runner1.name, 'Test for first runner - failed!')
+        self.all_results.append(result)
 
 
 if __name__ == '__main__':
